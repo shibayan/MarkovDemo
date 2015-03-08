e67f33fe024c10be1b0802a7aafc3e8e7c2b59bd
shibayan <shiba-yan@outlook.com>
2015-03-08 21:13:20 +0900

    initial commit

---
+++ MarkovDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace MarkovDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(@"C:\Users\shibayan\Documents\mecab\statemachine_data.txt");
+
+            var markovDic = new MarkovDictionary();
+
+            foreach (var line in lines)
+            {
+                markovDic.AddSentence(line.Split('|'));
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                var sentence = markovDic.BuildSentence();
+
+                Console.WriteLine(string.Join("", sentence));
+            }
+        }
+    }
+}
